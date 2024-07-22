bc063c57c3b9c5c52e446e04dc2750660159d9fa
shofique11 <programmingcode11@gmail.com>
2024-07-23 00:28:59 +0600

    method overloading

--- MethodOverloading/src/Main.java
+++ MethodOverloading/src/Main.java
@@ -1,15 +1,12 @@
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gu
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+      Calculator obj1 = new Calculator();
+      System.out.println(obj1.performAddition(50,60));
+      System.out.println(obj1.performAddition(40.5, 707));
+      System.out.println(obj1.performAddition(25.5, 20.5));
+      System.out.println(obj1.performAddition(20,30,50));
     }
 }
