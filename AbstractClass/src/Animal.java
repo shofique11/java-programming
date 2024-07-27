50dc335c2bcb2e846fc88260a336b0594ae60b2a
shofique11 <programmingcode11@gmail.com>
2024-07-27 18:11:47 +0600

    abstract class

---
+++ AbstractClass/src/Animal.java
@@ -0,0 +1,12 @@
+public abstract class Animal {
+    private final int legs;
+    private String name;
+
+    public Animal(int legs, String name) {
+        this.legs = legs;
+        this.name = name;
+    }
+    public String toString(){
+       return (name +" has " + legs + " legs");
+    }
+}
---
+++ AbstractClass/src/Tiger.java
@@ -0,0 +1,5 @@
+public class Tiger extends Animal{
+    public Tiger(){
+        super(4, "Tiger");
+    }
+}
